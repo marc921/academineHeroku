eed22b5c4a861d036c1a0c12075ddeaa6f2d92e3
Marc Brun <marc.brun@mines-paristech.fr>
2018-03-28 22:31:20 +0200

    Tries using server on other Heroku app

--- src/components/views/nav.jsx
+++ src/components/views/nav.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default class Nav extends React.Component {
+	componentDidMount() {
+	    this.callApi()
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	}
+
+	callApi = async () => {
+		const response = await fetch('/receiver');
+		const body = await response.json();
+
+		if (response.status !== 200) throw Error(body.message);
+
+		return body;
+	};
   
 	render(){
 		return(
