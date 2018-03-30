8a2a22495c1f30626856fbc3ad2fc769ae09f9ec
Marc Brun <marc.brun@mines-paristech.fr>
2018-03-30 15:07:51 +0200

    Removes s authentication on fetch

--- src/components/views/nav.jsx
+++ src/components/views/nav.jsx
@@ -8,14 +8,7 @@ export default class Nav extends React.Component {
 	}
 
 	callApi = async () => {
-		const response = await fetch('https://academine-backend.herokuapp.com/receiver', { 
-		   method: 'post', 
-		   headers: new Headers({
-		     'Authorization': 'Basic '+btoa('2a8i6w3h/q*:9i2z#486RI@e6nW5q'), 
-		     'Content-Type': 'application/x-www-form-urlencoded'
-		   }), 
-		   body: 'A=1&B=2'
-		 });
+		const response = await fetch('https://academine-backend.herokuapp.com/receiver');
 		const body = await response.json();
 
 		if (response.status !== 200) throw Error(body.message);
